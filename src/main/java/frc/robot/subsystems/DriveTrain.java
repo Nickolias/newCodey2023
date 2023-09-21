01bcfb7364ad0a31c3eaf440dee17abaa62e6e2c
ant-7802 <anttubemc@googlemail.com>
2023-09-21 15:00:54 -0400

    I do NOT have adhd

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -7,8 +7,11 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
 
 public class DriveTrain extends SubsystemBase {
   /** Creates the a new DriveTrain... */
@@ -16,18 +19,23 @@ public class DriveTrain extends SubsystemBase {
   private CANSparkMax motor1;
   private CANSparkMax motor2;
   private CANSparkMax motor3;
+  private MotorControllerGroup MotorGroup0;
   private MotorControllerGroup MotorGroup1;
-  private MotorControllerGroup MotorGroup2;
-
+  private DifferentialDrive drivetrain;
   public DriveTrain() {
     motor0 = new CANSparkMax(22,MotorType.kBrushless);
     motor1 = new CANSparkMax(22,MotorType.kBrushless);
     motor2 = new CANSparkMax(22,MotorType.kBrushless);
     motor3 = new CANSparkMax(22,MotorType.kBrushless);
-    MotorGroup1 = new MotorControllerGroup(motor0, motor1,motor2,motor3);
+    MotorGroup0 = new MotorControllerGroup(motor0, motor1);
     MotorGroup1 = new MotorControllerGroup(motor2,motor3);
+    drivetrain = new DifferentialDrive(MotorGroup0,MotorGroup1);
   }
   
+  public void drive(double forward, double turn) {
+    drivetrain.arcadeDrive(forward, turn, false);
+    System.out.println("I do not have adhd");
+  };
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
