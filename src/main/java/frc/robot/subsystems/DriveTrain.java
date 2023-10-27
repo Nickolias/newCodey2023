904538f34cc1b9efca1c4eb6311918a85a0fe67e
JoshB614 <144728059+JoshB614@users.noreply.github.com>
2023-10-27 18:49:34 +0000

    I made 2 variables named pancakeWaffleGyroBudaBudaBuh and bananaPeelSpiderBrainsOnKingJamesTheThirdOfFranceTheSecondOfNewGuinea

---
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -0,0 +1,39 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class DriveTrain extends SubsystemBase {
+  CANSparkMax motorBL, motorBR, motorFL, motorFR;
+  MotorControllerGroup motorControlL;
+  MotorControllerGroup motorControlR;
+  DifferentialDrive pancakeWaffleGyroBudaBudaBuh;
+  /** Creates a new DriveTrain. */
+  public DriveTrain() {
+    motorBL = new CANSparkMax(0, MotorType.kBrushless);
+    motorBR = new CANSparkMax(0, MotorType.kBrushless);
+    motorFL = new CANSparkMax(0, MotorType.kBrushless);
+    motorFR = new CANSparkMax(0, MotorType.kBrushless);
+    motorControlL = new MotorControllerGroup(motorBL,motorFL);
+    motorControlR = new MotorControllerGroup(motorBR,motorFR);
+    pancakeWaffleGyroBudaBudaBuh = new DifferentialDrive(motorControlL, motorControlR);
+  }
+  public void bananaPeelSpiderBrainsOnKingJamesTheThirdOfFranceTheSecondOfNewGuinea(double forwardSpeed, double turnSpeed){
+    pancakeWaffleGyroBudaBudaBuh.arcadeDrive(forwardSpeed, turnSpeed, false);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
+
